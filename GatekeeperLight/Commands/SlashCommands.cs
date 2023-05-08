9665ec7f86f87cf4e3de4cd1379e6e8f5214e7d5
Petr Tlapa <tlapik123@gmail.com>
2023-05-08 18:23:11 +0200

    Initial working commit

---
+++ GatekeeperLight/Commands/SlashCommands.cs
@@ -0,0 +1,19 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace GatekeeperLight.Commands; 
+
+public sealed class SlashCommands : ApplicationCommandModule {
+    private readonly RoleCheckAndGive _roleCheckAndGiveHandler;
+
+    public SlashCommands(RoleCheckAndGive roleCheckAndGiveHandler) {
+        _roleCheckAndGiveHandler = roleCheckAndGiveHandler;
+    }
+
+    [SlashCommand("verify", "Verify me based on a specific role (given by the admin) in a different server (given by the admin).")]
+    public async Task VerifyCommand(InteractionContext ctx) {
+        await _roleCheckAndGiveHandler.CheckAndGiveRole(ctx.User);
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("Executed the verify command."));
+    }
+}
---
+++ GatekeeperLight/RoleCheckAndGive.cs
@@ -0,0 +1,65 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+
+namespace GatekeeperLight;
+
+public class RoleCheckAndGive {
+    private readonly ulong _guildFromId;
+    private readonly ulong _guildFromRoleId;
+    private readonly ulong _guildToId;
+    private readonly ulong _guildToRoleId;
+
+    private DiscordGuild? _guildFrom;
+    private DiscordRole? _guildFromRole;
+    private DiscordGuild? _guildTo;
+    private DiscordRole? _guildToRole;
+
+    public RoleCheckAndGive(ulong guildFromId, ulong guildFromRoleId, ulong guildToId, ulong guildToRoleId) {
+        _guildFromId = guildFromId;
+        _guildFromRoleId = guildFromRoleId;
+        _guildToId = guildToId;
+        _guildToRoleId = guildToRoleId;
+    }
+    
+    /// <summary>
+    /// When the guilds are downloaded populate the guild and role fields.
+    /// </summary>
+    /// <param name="_">DC client (unused)</param>
+    /// <param name="e">Guilds that the bot is in.</param>
+    /// <returns><see cref="Task"/>Completed task.</returns>
+    public Task GuildsDownloadCompletedHandler(DiscordClient _, GuildDownloadCompletedEventArgs e) {
+        _guildFrom = e.Guilds[_guildFromId];
+        _guildTo = e.Guilds[_guildToId];
+
+        _guildFromRole = _guildFrom.GetRole(_guildFromRoleId);
+        _guildToRole = _guildTo.GetRole(_guildToRoleId);
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// Check if the user has a configured role in a configured server and give him a configured role in configured server.  
+    /// </summary>
+    /// <param name="user">User to check and give a role to.</param>
+    public async Task CheckAndGiveRole(DiscordUser user) {
+        if (_guildFromRole is null || _guildToRole is null || _guildFrom is null || _guildTo is null) {
+            // Object has not yet been initialized
+            return;
+        }
+
+        var guildFromMember = await _guildFrom.GetMemberAsync(user.Id);
+        var guildToMember = await _guildTo.GetMemberAsync(user.Id);
+        if (user.IsBot || guildFromMember is null || guildToMember is null) {
+            // TODO: somehow do the error handling
+            return;
+        }
+
+        if (!guildFromMember.Roles.Contains(_guildFromRole)) {
+            // TODO: user doesnt have the required role
+            return;
+        }
+
+        await guildToMember.GrantRoleAsync(_guildToRole,
+            $"Also has role: {_guildFromRole.Name} in {_guildFrom.Name} server.");
+    }
+}
